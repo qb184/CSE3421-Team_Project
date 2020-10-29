64ffc4325b5a8d3b41237672259483aff8e504ce
dhdavidkim1204@gmail.com <dhdavidkim1204@gmail.com>
2020-10-29 16:38:49 -0400

    Add data input BB_EMPLOYEE

---
+++ Sample Data Input/Data_Input_BB_EMPLOYEE.sql
@@ -0,0 +1,23 @@
+INSERT INTO BB_EMPLOYEE
+VALUES	
+    ('E0001', 'Michael Scott',  'Schedule', 'scott1@gmail.com', 2162343212, '2015-12-12', 70000),
+    ('E0002', 'Dwight Schrute',  'Schedule', 'schrute09@gmail.com', 21635679002, '2017-04-12', 60000),
+    ('E0003', 'Jim Halpert',  'Schedule', 'halpert123@gmail.com', 4366576678, '2017-06-01', 60000),
+    ('E0004', 'Pam Beasley',  'Schedule', 'pampam@gmail.com',6142344455, '2017-08-04', 50000),
+    ('E0005', 'Andy Bernard', 'Schedule', 'bernard23@gmail.com', 6148762749, '2018-01-12', 55000),
+    ('E0006', 'Richard Korf', 'Schedule', 'korh13@gmail.com', 6141358271, '2018-05-15', 85000),
+    ('E0007', 'John Kim', 'Schedule', 'kim938@gmail.com', 6032948212, '2019-10-18', 70000),
+    ('E0008', 'Joseph Kay', 'Schedule', 'kay81@gmail.com', 2163328172, '2017-08-03', 55000),
+    ('E0009', 'Allen Klinger', 'Schedule', 'klinger05@gmail.com', 6149302827, '2017-06-23', 90000),
+    ('E0010', 'Paul Meyer', 'Schedule', 'meyer83@gmail.com', 6039281024, '2019-01-29', 60000),
+    ('E0011', 'James Johnson', 'Schedule', 'jj111@gmail.com', 3231219090, '2019-01-23', 45000),
+    ('E0012', 'Jake Jackson', 'Schedule', 'jj121@gmail.com', 2341256679, '2020-02-21', 80000),
+    ('E0013', 'Tom Smith', 'Schedule', 'ts33@gmail.com', 2379346784, '2017-05-30', 55000),
+    ('E0014', 'Greg Brown', 'Schedule', 'gb1256@gmail.com', 3356789345, '2019-09-30', 70000),
+    ('E0015', 'Tim Simpson', 'Schedule', 'ts6543@gmail.com', 1005466783, '2018-04-23', 100000),
+    ('E0016', 'Jerry Jackson', 'Schedule', 'jj123@gmail.com', 6147958888, '2020-05-05', 50000),
+    ('E0017', 'Jarrod Newsted', 'Schedule', 'jn123@gmail.com', 6147958878, '2016-05-30', 51000),
+    ('E0018', 'Jena Jackson', 'Schedule', 'jo113@gmail.com', 6147958688, '2017-05-05', 53000),
+    ('E0019', 'James Hatfield', 'Schedule', 'js144@gmail.com', 6147956666, '2019-05-05', 48000),
+    ('E0020', 'Jerry Schemit', 'Schedule', 'js155@gmail.com', 6147977775, '2018-05-05', 46000);
+
