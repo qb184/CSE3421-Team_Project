bde651e957c19a091017a257678c4d23e269a398
qb184 <quynhbui184@gmail.com>
2020-11-05 16:16:37 -0500

    updated 5a, 5b

--- Simple Queries/Q5-a.sql
+++ Simple Queries/Q5-a.sql
@@ -3,6 +3,6 @@
 	SELECT	A.Name, SUM (P.Price)
 	FROM		ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
 	WHERE	A.Account_id=B.Account_id AND B.Account_id=O.Account_id
-			AND O.Transaction_id = H.Transaction_id AND P.Product_id=H.Produc_id 
+			AND O.Transaction_id = H.Transaction_id AND P.Product_id=H.Product_id 
 	
 	GROUP BY	A.Name;
--- Simple Queries/Q5-b.sql
+++ Simple Queries/Q5-b.sql
@@ -1,2 +1,9 @@
 /* b. Provide a list of buyer names and e-mail addresses for buyers who have spent more than the average buyer. */
 
+SELECT	    A.Name, A.Email
+FROM		ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
+WHERE	    A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND O.Transaction_id = H.Transaction_id AND P.Product_id=H.Product_id
+GROUP BY	A.Name
+HAVING 	    SUM(price)>(SELECT 	    AVG(price)
+                    	FROM		ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
+				        WHERE	    A.Account_id=B.Account_id AND B.Account_id=O.Account_id B.Account_id=O.Account_idAND O.Transaction_id = H.Transaction_id AND P.Product_id=H.Product_id);
